0e1e4abbb557a4ecc8c158deed0d2200b1f0853a
Isak Törnros <isak.tornros@hotmail.com>
2016-03-24 21:14:30 +0100

    Added click event for Caspar Add button.

--- public/js/caspar_buttons.js
+++ public/js/caspar_buttons.js
@@ -1,3 +1,25 @@
+$('.caspar_add').click(function() {
+
+	// Get the data from the forms
+    var template =  $('#template').val();
+    var layer =  $('#layer').val();
+    var f0 = $('#f0').val();
+    var f1 = $('#f1').val();
+    var f2 = $('#f2').val();
+    var f3 = $('#f3').val();
+    var f4 = $('#f4').val();
+    var f5 = $('#f5').val();
+    var f6 = $('#f6').val();
+    console.log("Data from F6 taken.");
+
+    $(':button').prop('disabled', false);
+
+    // CG ADD Command
+    console.log("Calling CG_ADD...");
+    CG_ADD(layer, template, f0, f1, f2, f3, f4, f5, f6);  
+
+});
+
 $('.caspar_play').click(function() {
 
 	var layer = $('#layer').val();
