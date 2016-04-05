eafbc2e2fd841dc4faeadde3d070ebb72707fb16
Isak Törnros <isak.tornros@hotmail.com>
2016-04-05 16:47:46 +0200

    IP tweak

--- routes/router.rb
+++ routes/router.rb
@@ -1,16 +1,16 @@
 class Router < Sinatra::Base
 
 configure :production do
-    ENV['CASPARIP']="85.230.107.27"
+    ENV['CASPARIP']="192.168.10.126"
 end
 
 
 
-	# configure :development do
-	#      enable :logging
-	#      ENV['CASPARIP']="192.168.10.126"
-	#      ENV['SESSION_SECRET']="Nfoiyafljnaskuyi/T(&%FYgliuesgf)"
-	# end
+	configure :development do
+	     enable :logging
+	     ENV['CASPARIP']="192.168.10.126"
+	     ENV['SESSION_SECRET']="Nfoiyafljnaskuyi/T(&%FYgliuesgf)"
+	end
 
 	# configure :development do
 	#      enable :logging
