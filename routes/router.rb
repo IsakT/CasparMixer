bf7255cd1466795a06e6a1722b44d5423d069d0d
Isak Törnros <isak.tornros@hotmail.com>
2016-04-05 16:39:03 +0200

    Ip production

--- routes/router.rb
+++ routes/router.rb
@@ -1,16 +1,22 @@
 class Router < Sinatra::Base
 
+configure :production do
+    ENV['CASPARIP']="85.230.107.27"
+end
+
+
+
 	# configure :development do
 	#      enable :logging
 	#      ENV['CASPARIP']="192.168.10.126"
 	#      ENV['SESSION_SECRET']="Nfoiyafljnaskuyi/T(&%FYgliuesgf)"
 	# end
 
-		configure :development do
-	     enable :logging
-	     ENV['CASPARIP']="127.0.0.1"
-	     ENV['SESSION_SECRET']="Nfoiyafljnaskuyi/T(&%FYgliuesgf)"
-	end
+	# configure :development do
+	#      enable :logging
+	#      ENV['CASPARIP']="127.0.0.1"
+	#      ENV['SESSION_SECRET']="Nfoiyafljnaskuyi/T(&%FYgliuesgf)"
+	# end
 
   helpers Helper::CasparConnect
   helpers Helper::CasparXml
